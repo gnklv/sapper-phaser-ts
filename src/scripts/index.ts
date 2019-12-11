b334d15430f8b53e6d490d4fd1165b1180861605
Georgy Nikolaev <wgg357@gmail.com>
2019-12-12 00:08:27 +0500

    Maked start scene

---
+++ src/scripts/index.ts
@@ -0,0 +1,10 @@
+import * as Phaser from "phaser";
+
+new Phaser.Game({
+    type: Phaser.AUTO,
+    parent: "sapper",
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: "#f0ffff",
+    scene: []
+});
---
+++ src/scripts/scenes/StartScene.ts
@@ -0,0 +1,42 @@
+const spritesheetPng = require("./../../assets/spritesheet.png");
+const spritesheetJson = require("./../../assets/spritesheet.json");
+
+enum Texts {
+    Title = "Sapper",
+    Caption = "Click anywhere to start"
+}
+
+enum Styles {
+    Color = "#008080",
+    Font = "Arial"
+}
+
+export class StartScene extends Phaser.Scene {
+    constructor() {
+        super("Start");
+    }
+
+    public preload(): void {
+        this.load.atlas("spritesheet", spritesheetPng, spritesheetJson);
+    }
+
+    public create(): void {
+        this.add.text(
+            this.cameras.main.centerX,
+            this.cameras.main.centerY - 100,
+            Texts.Title,
+            { font: `52px ${Styles.Font}`, fill: Styles.Color }
+        ).setOrigin(0.5);
+
+        this.add.text(
+            this.cameras.main.centerX,
+            this.cameras.main.centerY + 100,
+            Texts.Caption,
+            { font: `28px ${Styles.Font}`, fill: Styles.Color }
+        ).setOrigin(0.5);
+
+        this.input.once("pointerdown", () => {
+            this.scene.start("Game");
+        });
+    }
+}
